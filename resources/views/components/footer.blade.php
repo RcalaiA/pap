4d1c24059219b6ed71a139668726e6621e3d2c2f
RcalaiA <a115768@aeg1.pt>
2025-02-17 15:01:18 +0000

    Alterações nomeadamente gráficas + alterações nos DOC

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,36 +2,36 @@
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" class="flex items-center">
-                  <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
-                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+              <a href="/" class="flex items-center">
+                  <img src="{{ asset('images/logos/logo2.png') }}" class="h-12 me-7" alt="FlowBite Logo" />
               </a>
           </div>
-          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+          <!--Não esquecer, se depois eu precisar de adicionar colonas-->
+          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-1">
               <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Recursos</h2>
                   <ul class="text-gray-500 dark:text-gray-400 font-medium">
                       <li class="mb-4">
-                          <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
+                          <a href="https://flowbite.com/" class="hover:underline">Por prencher</a>
                       </li>
                       <li>
-                          <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
+                          <a href="https://tailwindcss.com/" class="hover:underline">Por prencher</a>
+                      </li>
+                  </ul>
+              </div>
+              <!--<div>
+                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">a</h2>
+                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                      <li class="mb-4">
+                          <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Por prencher</a>
+                      </li>
+                      <li>
+                          <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Por prencher</a>
                       </li>
                   </ul>
               </div>
               <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                      <li class="mb-4">
-                          <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Github</a>
-                      </li>
-                      <li>
-                          <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
-                      </li>
-                  </ul>
-              </div>
-              <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legalizações</h2>
                   <ul class="text-gray-500 dark:text-gray-400 font-medium">
                       <li class="mb-4">
                           <a href="#" class="hover:underline">Privacy Policy</a>
@@ -40,12 +40,12 @@
                           <a href="#" class="hover:underline">Terms &amp; Conditions</a>
                       </li>
                   </ul>
-              </div>
+              </div>-->
           </div>
       </div>
       <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a>Rui Calaia</a>
           </span>
           <div class="flex mt-4 sm:justify-center sm:mt-0">
               <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -3,7 +3,7 @@
     <nav class="border-gray-200 bg-white">
         <div class="container mx-auto flex flex-wrap items-center justify-between p-4">
         <a href="/" class="flex items-center">
-            <img src="{{ asset('images/logos/logo2.png') }}" alt="Literacias" class="h-10 mr-3">
+            <img src="{{ asset('images/logos/logo2.png') }}" alt="Literacias" class="h-12 mr-7">
             <!--<a href="/" class="flex items-center">-->
                 <!-- Novo logotipo -->
                 <!--<img src="logo.png" alt="Literacias" class="h-10 mr-3">-->
@@ -18,8 +18,8 @@
             <div class="hidden md:block w-full md:w-auto" id="mobile-menu">
                 <ul class="flex flex-col md:flex-row md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
                     <li>
-                        <a href="/sobre" class="bg-blue-700 md:bg-transparent text-white block pl-3 pr-4 py-2 md:text-blue-700 md:p-0 rounded">
-                            Sobre nós
+                        <a href="/literacias" class="bg-blue-700 md:bg-transparent text-white block pl-3 pr-4 py-2 md:text-blue-700 md:p-0 rounded">
+                            Literacias
                         </a>
                     </li>
                     <li class="relative">
@@ -55,13 +55,13 @@
         </div>
     </nav>
     <!-- LINHA PRETA -->
-    <div class="bg-black text-white">
+    <!--<div class="bg-black text-white">
         <div class="container mx-auto flex flex-wrap items-center justify-center p-2">
-            <a class="px-4 py-1 text-sm">--></a>
+            <a class="px-4 py-1 text-sm"></a>
             <a href="/html" class="px-4 py-1 text-sm hover:underline">Literacia Financeira</a>
             <a href="/css" class="px-4 py-1 text-sm hover:underline">Literacia Computacional</a>
             <a href="/css" class="px-4 py-1 text-sm hover:underline">Literacia Digital</a>
             <a class="px-4 py-1 text-sm"><--</a>
-        </div>
+        </div>-->
     </div>
 </div>
